7623a68e0f1745464801290132dfb7844e99e36a
Hung Hsieh <hung.hsieh@pontislaw.com>
2025-06-12 12:05:35 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,13 @@
+# 記帳程式
+
+products = []
+while True:
+	name = input("請輸入商品名稱：")
+	if name == "q":
+		break
+	price  = input("請輸入商品價格：")
+	products.append([name, price])
+	
+print(products)
+print(f"你總共建立了{len(products)}個商品的清單")
+print(f"商品{products[0][0]}價值{products[0][1]}元")
